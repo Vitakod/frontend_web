f7c3cfb0eef034e0b6a88f07e071c89211b6ac20
Renato Caçula <renatocacula2015@gmail.com>
2022-11-26 20:03:09 -0400

    Implementação de localização do dispositivo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import "./App.css";
 import ChatBot from "react-simple-chatbot";
+import { useGeolocated } from "react-geolocated";
+import { useState } from "react";
+
 function App() {
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } = useGeolocated(
+    {
+      positionOptions: {
+        enableHighAccuracy: false,
+      },
+      userDecisionTimeout: 5000,
+    }
+  )
+
+  const [localizacaoAtiva, setLocalizacaoAtiva] = useState(false)
+  const [usuarioLocalizacao, setUsuarioLocalizacao] = useState()
+
+  const verificarLocalizacao = () => {
+    if (coords) {
+      setLocalizacaoAtiva(true)
+      setUsuarioLocalizacao({ longitude: coords.longitude, latitude: coords.latitude })
+    }
+    else {
+      alert("Por favor click no canto superior esquerdo para conceder acesso a localização")
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+
         <ChatBot
+          style={{ display: localizacaoAtiva ? 'none' : '' }}
+          headerTitle="Verificando Localização"
+          handleEnd={verificarLocalizacao}
+          botDelay={2000}
+          steps={[
+            {
+              id: "1",
+              message:
+                "Antes de começarmos precisamos acessar sua localização :)",
+              trigger: "2"
+            },
+            {
+              id: "2",
+              options: [
+                { label: 'Entendi' }
+              ],
+              end: true
+            },
+          ]}
+        />
+
+        <ChatBot
+          style={{ display: !localizacaoAtiva ? 'none' : '' }}
           headerTitle="VITA=KOD"
           steps={[
             {
@@ -15,12 +64,12 @@ function App() {
             {
               id: "2",
               message:
-                "Antes de começarmos preciso que você nos conceda sua localização para o navegador :)",
-              trigger: "3",
+                "Obrigado por permitir a localização :)",
+              trigger: "3"
             },
             {
               id: "3",
-              user: true,
+              options: [{ label: 'Ok', trigger: '4' }],
               trigger: "4",
             },
             {
@@ -28,11 +77,13 @@ function App() {
               message: "Hi {previousValue}, nice to meet you!",
               end: true,
             },
-          ]}
-        />
+          ]
+          } />
+
       </header>
     </div>
-  );
+  )
+
 }
 
 export default App;
